5d57773182977877f0f1f88187fa458cf1066143
TsilaAllaoui <tsila.allaoui@gmail.com>
2023-06-20 18:59:10 +0300

    Fixed scroll to game card

--- src/components/GameCardList.tsx
+++ src/components/GameCardList.tsx
@@ -16,21 +16,6 @@ function GameCardList() {
 
   const [currentHoveredGame, setCurrentHoveredGame] = useState(0);
 
-  const [currentGame, setCurrentGame] = useState<HTMLDivElement | null>(null);
-
-//   function scrollToElement(element: HTMLDivElement) {
-//     var scrollableDiv = document.querySelector("#game-card-list-container");
-//     const containerRect = scrollableDiv!.getBoundingClientRect();
-//   const elementRect = element.getBoundingClientRect();
-
-//   console.log("containerRect:" + containerRect.left + ":" + containerRect.right);
-//   console.log("elementRect:" + elementRect.left + ":" + elementRect.right);
-
-//   if (elementRect.right > containerRect.right) {
-//     scrollableDiv!.scrollLeft += elementRect.right - containerRect.right;
-//   }
-//   }
-
   const containerOnKeydown = (e: KeyboardEvent) => {
     console.log(e.key);
     if (e.key == "ArrowLeft") {
@@ -47,27 +32,25 @@ function GameCardList() {
 
   useEffect(() => {
     let cards: NodeListOf<HTMLDivElement> = document.querySelectorAll(".game-card");
-    let currentGame: HTMLDivElement|null = cards[5];
     for (const element of cards) {
-        if (element.style.border == "5px solid rgb(64, 206, 195)") {
-            setCurrentGame(element);
-            // scrollToElement(currentGame!);
-            let rect = element.getBoundingClientRect();
-            // console.log(rect.left + ":" + rect.right + "/" + rect.top + ":" + rect.bottom);
-            const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-            if (rect.left > screenWidth){
-                console.log("overflow");
-                element.scrollIntoView();
-            }
-            break;
+      if (element.style.border == "5px solid rgb(64, 206, 195)") {
+        let rect = element.getBoundingClientRect();
+        const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        if (rect.left + rect.width > screenWidth) {
+          element.scrollIntoView({ behavior: "smooth" });
         }
+        if (rect.left < 0) {
+          element.scrollIntoView({ behavior: "smooth", });
+        }
+        break;
+      }
     }
   }, [currentHoveredGame]);
 
   return (
     <div id="game-card-list-container">
       {gameList.map((game, index) => (
-        <GameCard title={game.title} color={game.color} key={game.title} active={index == currentHoveredGame}/>
+        <GameCard title={game.title} color={game.color} key={game.title} active={index == currentHoveredGame} />
       ))}
     </div>
   );
