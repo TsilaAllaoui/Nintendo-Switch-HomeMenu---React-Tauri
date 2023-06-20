7de63c4f1fe86401fa6512b446e2eaeb89eecc84
TsilaAllaoui <tsila.allaoui@gmail.com>
2023-06-20 14:32:01 +0300

    Working date

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,14 +4,25 @@ import { RiBatteryLowFill } from "react-icons/ri";
 import Buttons from "./Buttons";
 import GameCardList from "./GameCardList";
 import icon from "../assets/icon.png";
+import { useEffect, useState } from "react";
 
 function Home() {
+
+    const [currentTime, setCurrentTime] = useState<string>(Date());
+
+    useEffect(() => {
+      setInterval(() => {
+        setCurrentTime(Date());
+      }, 1000)
+    }, [])
+    
+
     return <div id="home-root">
        <header>
         <div id="user" style={{backgroundImage: `url(${icon})`}}>
         </div>
         <div id="system-info">
-            <p>12:39PM</p>
+            <p>{currentTime.split(" ")[4]}</p>
             <MdWifi id="wifi-icon"/>
             <p>77%</p>
             <RiBatteryLowFill id="battery-icon"/>
